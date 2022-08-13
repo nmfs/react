72f6b1186b6f2a47b83ee56c0cee9e335c91619a
nmfs <nuno.freitas.silva@gmail.com>
2022-08-13 13:14:13 +0100

    1.8

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,12 +1,25 @@
 import {useState} from 'react'
 
+const Statistics = ({good, neutral, bad}) => {
+  const all = () => good + neutral + bad
+  return (
+    <>
+     <h1> statistics</h1>
+      <div>good {good}</div>
+      <div>neutral {neutral}</div>
+      <div>bad {bad}</div>
+      <div>all {all()}</div>
+      <div>average {(good - bad)/ all()}</div>
+      <div>positive {100 * good/ all()} %</div>
+    </>
+  )
+}
 
 function App() {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  const all = () => good + neutral + bad
 
 
   return (
@@ -15,13 +28,7 @@ function App() {
       <button onClick={() => setGood(good + 1)}>good</button>
       <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
       <button onClick={() => setBad(bad + 1)}>bad</button>
-      <h1> statistics</h1>
-      <div>good {good}</div>
-      <div>neutral {neutral}</div>
-      <div>bad {bad}</div>
-      <div>all {all()}</div>
-      <div>average {(good - bad)/ all()}</div>
-      <div>positive {100 * good/ all()} %</div>
+      <Statistics good={good} neutral={neutral} bad={bad}/>
     </div>
   );
 }
