2c39923fa10b093438c35f8bf41b646ede7352bb
nmfs <nuno.freitas.silva@gmail.com>
2022-08-16 11:21:48 +0100

    2.7

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,56 @@
+import { useState } from 'react'
+
+const Person = ({person}) => {
+  return (
+    <li>{person.name}</li>
+  )
+}
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas'}
+  ])
+  const [newName, setNewName] = useState('')
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    const personObject = {
+      name: newName,
+    }
+    let found = false
+    persons.forEach(person => {
+        if (person.name === newName) found = true
+      }
+    )
+    if (found) {
+      alert(newName + ' is already added to phonebook')
+      return
+    }
+    setPersons(persons.concat(personObject))
+    setNewName('')
+  }
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input onChange={handleNameChange}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map(person => <Person key={person.name} person={person}/>)}
+      </ul>
+    </div>
+  )
+}
+
+export default App
