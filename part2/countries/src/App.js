d05946af4c153dfd67799fddee45481139f17c00
nmfs <nuno.freitas.silva@gmail.com>
2022-08-16 17:19:07 +0100

    2.12

---
+++ part2/countries/src/App.js
@@ -0,0 +1,63 @@
+import { useState, useEffect} from "react";
+import axios from 'axios'
+
+const ShowCountries = ({countries}) => {
+  if(countries.length > 10)
+    return (<div>Too many matches, specify another filter</div>)
+  else if(countries.length > 1)
+    return (
+      <ul>
+        {countries.map(country => <li key={country.name.common}>{country.name.common}</li>)}
+      </ul>
+    )
+  else if(countries.length === 0)
+    return (<div>No match, specify another filter</div>)
+  else {
+    const languages = Object.values(countries[0].languages)
+    return (
+     <div>
+        <h1>{countries[0].name.common}</h1>
+        <div>capital {countries[0].capital[0]}</div>
+        <div>area {countries[0].area}</div>
+        <h2>languages:</h2>
+        <ul>
+          {languages.map(lang => <li key={lang}>{lang}</li>)}
+        </ul>
+        <img src={countries[0].flags.png} width="200"/>
+      </div>
+    )
+  }
+}
+
+function App() {
+  const [countries, setCountries] = useState([])
+  const [pattern, setPattern] = useState('')
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/countries')
+      .then(response => {
+        setCountries(response.data)
+      })
+  }, [])
+
+
+  const handleFilter = (event) => {
+    setPattern(event.target.value)
+  }
+
+  const filteredCountries = countries.filter(
+    country => country.name.common.toUpperCase().indexOf(pattern.toUpperCase()) !== -1
+  )
+
+  return (
+    <>
+      <div>
+        find countries <input onChange={handleFilter} />
+      </div>
+      <ShowCountries countries={filteredCountries} />
+    </>
+  );
+}
+
+export default App;
